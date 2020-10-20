ff8bc261aa3630dd20561c51cd88500e176ebed4
“heyaling495 <1360462218@qq.com>
2020-10-20 15:54:36 +0800

    首次提交首页内容，加测试git推送功能

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,8 +1,41 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view class="container">
+		<view class="banner">
+			<image src="../../static/images/index/index-banner.jpg" mode="" class="bg"></image>
+		</view>
+		<view class="content">
+			<view class="entrance">
+				<view class="item">
+					<image src="../../static/images/index/eat-in.png" mode="" class="icon"></image>
+					<view class="title">
+						堂食
+					</view>
+				</view>
+				<view class="item">
+					<image src="../../static/images/index/take-out.png" mode="" class="icon"></image>
+					<view class="title">
+						外卖
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="info">
+			<view class="integral_section">
+				<view class="top">
+					<view class="title">
+						我的积分
+					</view>
+					<view class="banner">
+						999
+					</view>
+				</view>
+				<view class="bottom">
+					进入积分商城兑换优惠券或周边
+				</view>
+			</view>
+			<view class="qrcode_section">
+				
+			</view>
 		</view>
 	</view>
 </template>
@@ -11,7 +44,7 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: 'HeHe-Shop'
 			}
 		},
 		onLoad() {
@@ -23,30 +56,64 @@
 	}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+<style lang='scss' scoped>
+	.container {
+		.banner {
+			position: relative;
+			width: 100%;
+			height: 480rpx;
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
+			.bg {
+				width: 100%;
+				height: 480rpx;
+			}
+		}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+		.content {
+			padding: 0 32rpx;
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+			.entrance {
+				position: relative;
+				background-color: $uni-bg-color;
+				border-radius: 10rpx;
+				margin-top: -80rpx;
+				box-shadow: $box-shadow;
+				padding: 32rpx 0;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+
+				.item {
+					flex: 1;
+					display: flex;
+					flex-direction: column;
+					align-items: center;
+					position: relative;
+
+					&:nth-child(1):after {
+						content: '';
+						position: absolute;
+						width: 1rpx;
+						background-color: #ddd;
+						right: 0;
+						height: 100%;
+						transform: scaleX(0.5) scaleY(0.8);
+					}
+
+					.icon {
+						width: 80rpx;
+						height: 84rpx;
+						margin-bottom: 8rpx;
+					}
+
+					.title {
+						color: $uni-text-color;
+					}
+				}
+			}
+			.info{
+				background-color: $uni-bg-color;
+			}
+		}
 	}
 </style>
